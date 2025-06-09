f841c940d647aaa6883befb6fe54275ec9251a95
eirikek <eirik.engen.kvam@gmail.com>
2025-06-09 15:07:05 +0200

    feat: enhance hover interaction in WeekCell component with background overlay

--- frontend/src/components/ui/program/week/WeekCell.tsx
+++ frontend/src/components/ui/program/week/WeekCell.tsx
@@ -13,29 +13,32 @@ export default function WeekCell({ event, isLast }: Props) {
   const [hover, setHover] = useState(false)
 
   return (
-    <Link href={`/program/${event.slug}`} className="block">
-      <div
-        className={`min-w-[256px] text-center relative overflow-hidden border-t border-x border-secondary p-[10px] cursor-pointer group ${
-          isLast ? 'border-b' : ''
-        }`}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
+      <Link href={`/program/${event.slug}`} className="block">
         <div
-          className={`absolute inset-0 bg-cover bg-center transition-opacity duration-200 ${
-            hover ? 'opacity-60' : 'opacity-0'
-          }`}
-          style={{ backgroundImage: `url(${event.image})` }}
-        />
+            className={`min-w-[256px] text-center relative overflow-hidden border-t border-x border-secondary p-[10px] cursor-pointer group ${
+                isLast ? 'border-b' : ''
+            }`}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
+          {hover && (
+              <div className="absolute inset-0">
+                <div
+                    className="w-full h-full bg-cover bg-center"
+                    style={{ backgroundImage: `url(${event.image})` }}
+                />
+                <div className="absolute inset-0 bg-black opacity-50" />
+              </div>
+          )}
 
-        <h3 className="relative z-10 text-calendar-title">{event.title}</h3>
-        {event.performer && (
-          <p className="relative z-10 text-calendar-time font-bold">{event.performer}</p>
-        )}
-        <p className="relative z-10 text-calendar-time">
-          <span className="font-bold">{event.startTime}</span> {event.location}
-        </p>
-      </div>
-    </Link>
+          <h3 className={`relative z-10 text-calendar-title  ${hover ? 'text-white' : ''}`}>{event.title}</h3>
+          {event.performer && (
+              <p className={`relative z-10 text-calendar-time font-bold ${hover ? 'text-white' : ''}`}>{event.performer}</p>
+          )}
+          <p className={`relative z-10 text-calendar-time  ${hover ? 'text-white' : ''}`}>
+            <span className="font-bold">{event.startTime}</span> {event.location}
+          </p>
+        </div>
+      </Link>
   )
 }
