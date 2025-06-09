2c997b3bfa4c461a145a2451e36b5e1d7f098d7b
eirikek <eirik.engen.kvam@gmail.com>
2025-06-09 21:47:37 +0200

    feat: add ArchiveContainer and ArchiveItem components for displaying archived links

---
+++ frontend/src/components/ui/archive/ArchiveItem.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { HiArrowLongRight } from 'react-icons/hi2'
+
+interface Props {
+    title: string
+    link: string
+    isLast?: boolean
+}
+
+export default function ArchiveItem({ title, link, isLast }: Props) {
+    return (
+        <div className={`border-t border-secondary ${isLast ? 'border-b' : ''}`}>
+            <a
+                className="py-[10px] tablet:px-[20px] flex gap-[5px] justify-center items-center tablet:hover:bg-secondary tablet:hover:text-primary transition-all duration-300 cursor-pointer"
+                href={link}
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                <h3 className="text-h3">{title}</h3>
+                <p className="flex items-center  text-h6">
+                    <HiArrowLongRight
+                        size={30}
+                    />
+                </p>
+            </a>
+        </div>
+    )
+}
