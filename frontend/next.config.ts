97404576ccc3f8d38559c787cbf42c1509f36a56
Christopher Høe <christopher.hoee@gmail.com>
2025-06-24 11:22:48 +0200

    fix: headers

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,6 +12,23 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/api/draft-mode/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            // must exactly match your Studio origin (no wildcards!)
+            value: 'https://hostscena.sanity.studio',
+          },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+      {
+        // still need to let Studio iframe your pages
         source: '/(.*)',
         headers: [
           {
--- frontend/src/app/api/draft-mode/enable/route.ts
+++ frontend/src/app/api/draft-mode/enable/route.ts
@@ -1,7 +1,19 @@
 import { client } from '@/sanity/lib/client'
 import { token } from '@/sanity/lib/token'
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
+import { NextResponse } from 'next/server'
 
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({ token }),
 })
+
+export const OPTIONS = () => {
+  return NextResponse.json(null, {
+    headers: {
+      'Access-Control-Allow-Origin': 'https://hostscena.sanity.studio',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+}
