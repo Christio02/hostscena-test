f02a85d1d4f07f52987ef1d358779a5f1b224790
Christopher Høe <christopher.hoee@gmail.com>
2025-06-24 13:34:20 +0200

    preview secret

--- frontend/src/app/api/draft-mode/enable/route.ts
+++ frontend/src/app/api/draft-mode/enable/route.ts
@@ -1,7 +1,20 @@
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import { client } from '@/sanity/lib/client'
 import { token } from '@/sanity/lib/token'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const { GET } = defineEnableDraftMode({
+export const { GET: _enable } = defineEnableDraftMode({
   client: client.withConfig({ token }),
 })
+const PREVIEW_SECRET = process.env.SANITY_PREVIEW_SECRET!
+
+export async function GET(req: NextRequest) {
+  // 1) check the query param yourself
+  const urlSecret = new URL(req.url).searchParams.get('secret')
+  if (urlSecret !== PREVIEW_SECRET) {
+    return NextResponse.json('Invalid secret', { status: 401 })
+  }
+
+  // 2) forward the (same) request to the helper
+  return _enable(req)
+}
